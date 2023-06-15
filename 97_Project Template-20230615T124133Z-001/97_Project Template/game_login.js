f97b6cd02456f37805e63bd5a1b35c0e595d2b17
divyapravyansh <133664602+divyapravyansh@users.noreply.github.com>
2023-06-15 18:36:40 +0530

    Add files via upload

---
+++ 97_Project Template-20230615T124133Z-001/97_Project Template/game_login.js
@@ -0,0 +1,10 @@
+// Create function addUser()
+function ____ {
+  // Get value of user by id player1_name_input and player2_name_input
+  user_name = document.getElementById("player_1 , player_2").ariaValueMax;
+  // Store these values locally
+  localStorage.setItem("player1_value", "playervalue");
+  //Assign "game_page.html" to window.location
+  window.location = "game_page.item"
+}
+
